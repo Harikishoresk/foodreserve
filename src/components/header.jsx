0a42d80fecf788d8757727734bc691142cc298b3
RidhanParvendhan <ridhanparvendhan@gmail.com>
2023-05-17 11:53:20 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-export const Header = {} => {
+export const Header = () => {
     return (
         <nav id="header" className="bg-black text-white">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-content-between mt-0 py-2">
